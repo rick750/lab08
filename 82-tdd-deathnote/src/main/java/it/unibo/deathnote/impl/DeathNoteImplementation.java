a281857fac60f9deb52f422ccf29cb61e272b0f8
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-11-11 11:50:12 +0100

    Created implementation of DeathNote interface

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -0,0 +1,52 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+/**
+ * DeathNoteImplementation
+ */
+public class DeathNoteImplementation implements DeathNote{
+
+    @Override
+    public String getDeathCause(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getRule(int ruleNumber) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void writeName(String name) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
