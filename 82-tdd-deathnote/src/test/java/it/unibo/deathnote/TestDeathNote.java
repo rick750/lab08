55a2f5adc8d553910dd6c8c1aead2d9b3cf4e051
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-11-11 13:58:10 +0100

    Implemented test for invalid rules

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,41 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
 class TestDeathNote {
+    private static final int ZERO = 0;
+    private static final int NEG_VALUE = -1;
+    private DeathNoteImplementation dn;
+
+    @BeforeEach
+    void sitUp(){
+        this.dn = new DeathNoteImplementation();
+    }
+
+    @Test
+    void TestInvalidRules() {
+        try {
+            dn.getRule(ZERO);
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertEquals(dn.getRule(ZERO), e);
+            assertFalse(e.getMessage().isBlank());
+        }
+        try {
+            dn.getRule(NEG_VALUE);
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertEquals(dn.getRule(NEG_VALUE), e);
+            assertFalse(e.getMessage().isBlank());
+        }
+    } 
 
 }
