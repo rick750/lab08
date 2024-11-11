9b29ef33bd8e8590533d8372255108e8234440d4
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-11-11 11:46:32 +0100

    Removed useless import

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -3,7 +3,6 @@ package it.unibo.mvc;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
