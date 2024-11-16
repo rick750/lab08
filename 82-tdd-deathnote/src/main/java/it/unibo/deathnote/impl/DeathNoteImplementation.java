a32c1373208f413cd289ea3b8b7efe530e824576
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-16 18:10:54 +0100

    Implemented writeCause and writeDetails

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -10,64 +10,81 @@ import it.unibo.deathnote.api.DeathNote;
  */
 public class DeathNoteImplementation implements DeathNote{
 
-    Map <String, Death> deathnote = new HashMap<>();
-
-    @Override
-    public String getDeathCause(String name) {
-        throw new IllegalArgumentException("The provided name is not written in this DeathNote");
-    }
-
-    @Override
-    public String getDeathDetails(String name) {
-        throw new IllegalArgumentException("The provided name is not written in this DeathNote");
-    }
-
-    @Override
-    public String getRule(int ruleNumber) {
-        if (ruleNumber <= 0 || ruleNumber > RULES.size()) {
-            throw new IllegalArgumentException("The given rule number is smaller than 1 or larger than the number of rule");
+    private static final int CAUSE_TIME = 40;    
+    private static final int DETAILS_TIME = 6040;
+    
+        Map <String, Death> deathnote = new HashMap<>();
+        private String lastName;
+    
+        @Override
+        public String getDeathCause(String name) {
+            throw new IllegalArgumentException("The provided name is not written in this DeathNote");
         }
-        return RULES.get(ruleNumber - 1);        
-    }
-
-    @Override
-    public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean writeDeathCause(String cause) {
-        throw new IllegalStateException("There is no name written in this DeathNote, or the cause is null");
-    }
-
-    @Override
-    public boolean writeDetails(String details) {
-        throw new IllegalStateException("There is no name written in this DeathNote, or the cause is null");
-    }
-
-    @Override
-    public void writeName(String name) {
-        if (name == null) {
-            throw new NullPointerException("The given name is null");
+    
+        @Override
+        public String getDeathDetails(String name) {
+            throw new IllegalArgumentException("The provided name is not written in this DeathNote");
         }
-        deathnote.put(name, null);
-    }
+    
+        @Override
+        public String getRule(int ruleNumber) {
+            if (ruleNumber <= 0 || ruleNumber > RULES.size()) {
+                throw new IllegalArgumentException("The given rule number is smaller than 1 or larger than the number of rule");
+            }
+            return RULES.get(ruleNumber - 1);        
+        }
+    
+        @Override
+        public boolean isNameWritten(String name) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+    
+        @Override
+        public boolean writeDeathCause(String cause) {
+            if (cause == null || deathnote.isEmpty()) {
+                throw new IllegalStateException("There is no name written in this DeathNote, or the cause is null");
+            }
+            if (getCurrentTime() < deathnote.get(this.lastName).time + CAUSE_TIME) {
+                deathnote.get(this.lastName).setCause(cause);
+                return true;
+            }
+            return false;
+        }
+    
+        @Override
+        public boolean writeDetails(String details) {
+            if (details == null || deathnote.isEmpty()) {
+                throw new IllegalStateException("There is no name written in this DeathNote, or the cause is null");
+            }
+            if (getCurrentTime() < deathnote.get(this.lastName).time + DETAILS_TIME) {
+				deathnote.get(this.lastName).setCause(details);
+				return true;
+			}
+			return false;
+    	}
 
-    private static long getCurrentTime() {
-        return System.currentTimeMillis();
-    }
+		@Override
+		public void writeName(String name) {
+			if (name == null) {
+				throw new NullPointerException("The given name is null");
+			}
+			deathnote.put(name, null);
+			this.lastName = name;
+		}
+
+			private static long getCurrentTime() {
+		return System.currentTimeMillis();
+		}
 
     private static class Death {
 
 
         private static final String EMPTY_STRING = "";
         private static final String DEF_CAUSE = "heart attack";
-        private final String cause;
-        private final String details;
+        private String cause;
+        private String details;
         private final long time;
-
-        
 
         private Death(final String cause, final String details) {
             this.cause = cause;
@@ -86,7 +103,16 @@ public class DeathNoteImplementation implements DeathNote{
 
         public String getDetails() {
             return this.details;
-        }             
+        }   
+        
+        public void setCause(String cause) {
+            this.cause = cause;
+        }
+
+        public void setDetails(String details) {
+            this.details = details;
+        }
+
     }
     
 }
